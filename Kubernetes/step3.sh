d78af46b13b44418be7ae9ad2bda290b246d5c74
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-06-13 02:37:09 +0700

    Update step3.sh

--- Kubernetes/step3.sh
+++ Kubernetes/step3.sh
@@ -7,7 +7,7 @@ sudo apt -y install curl apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
-sudo apt -y install vim git curl wget kubelet kubeadm kubectl
+sudo apt -y install kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 #Configure sysctl on master and workers
 sudo modprobe overlay
