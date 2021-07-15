36fda0fcd008ae856f0c57ca7e961412617eb4c1
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-07-16 05:23:39 +0700

    Update script.sh

--- first-network/scripts/script.sh
+++ first-network/scripts/script.sh
@@ -25,7 +25,7 @@ LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
 COUNTER=1
 MAX_RETRY=10
 
-CC_SRC_PATH="github.com/chaincode/fabcar/javascript"
+CC_SRC_PATH="github.com/chaincode/fabcar/java"
 if [ "$LANGUAGE" = "node" ]; then
 	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/fabcar/javascript/"
 fi
